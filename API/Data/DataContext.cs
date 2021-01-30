5d68f00ee61fdd815ff2614ac68a74bff4a3727a
ThomasL <thomas_641991@hotmail.com>
2021-01-30 20:53:59 +0100

    End of section 14

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -10,5 +10,27 @@ namespace API.Data
         }
 
         public DbSet<AppUser> Users { get; set; }
+        public DbSet<UserLike> Likes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //This line is to prevent errors when try to add a migrating
+            base.OnModelCreating(builder);
+
+            // Create the primary key for the table
+            builder.Entity<UserLike>().HasKey(k => new { k.SourceUserId, k.LikedUserId });
+
+            builder.Entity<UserLike>()
+                .HasOne(s => s.SourceUser)
+                .WithMany(l => l.LikedUsers)
+                .HasForeignKey(s => s.SourceUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<UserLike>()
+                .HasOne(s => s.LikedUser)
+                .WithMany(l => l.LikedByUsers)
+                .HasForeignKey(s => s.LikedUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
